cc0d65cbbd731f1e9751f152fb6f81d31a8d4156
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-28 16:03:22 -0600

    created fragment

---
+++ pages/[pid].js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+
+
+function ProductDetailPage() {
+    return <Fragment>
+        <h1>Title</h1>
+        <p>Description</p>
+    </Fragment>
+}
+
+export default ProductDetailPage;
