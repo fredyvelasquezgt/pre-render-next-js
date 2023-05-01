88730c34ce1a0ce3ce6be56f52cfb367c1ad5cf1
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-30 21:04:11 -0600

    quit useEffect

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,30 +1,34 @@
 import { useEffect, useState } from "react";
+import useSWR from 'swr'
 
 function LastSalesPage() {
   const [sales, setSales] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch("https://nextjs-course-40c91-default-rtdb.firebaseio.com/sales.json")
-      .then((response) => response.json()) //aqui accedo a la informacion
-      .then((data) => {
-        //aqui ya puedo manipular la informacion
+   const {data, error} =  useSWR('https://nextjs-course-40c91-default-rtdb.firebaseio.com/sales.json');
 
-        //asi transformo la data que viene de un objecto y la vuelvo un array
-        const transformedSales = [];
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        //aqui meto el array ya con la data transformada
-        setSales(transformedSales);
-        setIsLoading(false); //aqui ya no cargo nada
-      });
-  }, []);
+
+//   useEffect(() => {
+//     setIsLoading(true);
+//     fetch("https://nextjs-course-40c91-default-rtdb.firebaseio.com/sales.json")
+//       .then((response) => response.json()) //aqui accedo a la informacion
+//       .then((data) => {
+//         //aqui ya puedo manipular la informacion
+
+//         //asi transformo la data que viene de un objecto y la vuelvo un array
+//         const transformedSales = [];
+//         for (const key in data) {
+//           transformedSales.push({
+//             id: key,
+//             username: data[key].username,
+//             volume: data[key].volume,
+//           });
+//         }
+//         //aqui meto el array ya con la data transformada
+//         setSales(transformedSales);
+//         setIsLoading(false); //aqui ya no cargo nada
+//       });
+//   }, []);
 
   if (isLoading) {
     return <p>Loading...</p>;
